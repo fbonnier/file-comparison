9a131142f259116558802161df49fcac9f7799be
Florent B <75836984+fbonnier@users.noreply.github.com>
2021-07-22 11:16:10 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "profile",
+    "nilsimsa",
+    "fuzzywuzzy",
+    "json",
+    "argparse",
+    "neo"
+]
+
+setup(
+    name='file_comparison',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/fbonnier/file_comparison.git',
+    license="CeCILL",
+    author='Florent Bonnier',
+    author_email='florent.bonnier@cnrs.fr',
+    description='File Comparison Methods Suit',
+    # Requirements
+    install_requires=install_requires,
+)
