058c7c512b1890c4d988ff03f530a30898ffaaf6
Florent B <75836984+fbonnier@users.noreply.github.com>
2021-09-13 11:40:39 +0200

    Add bijective search mode

--- file_comparison/npz.py
+++ file_comparison/npz.py
@@ -5,17 +5,36 @@ from collections.abc import Iterable
 import neo.io
 import json
 
-
+nb_values_total = 0
+nb_errors = 0
 all_failures = {}
 known_types = [np.lib.npyio.NpzFile, np.ndarray, neo.core.block.Block, neo.core.Segment, str, bytes, list, dict, bool, float, int, neo.core.spiketrain.SpikeTrain, neo.core.analogsignal.AnalogSignal,]
+
+def compute_ratio ():
+    print ("Number of errors = " + str (nb_errors))
+    print ("Number of values = " + str (nb_values_total))
+    print ("Number of failures = " + str (len(all_failures)) + "\n")
+
+    for ifail in all_failures:
+        print (type(ifail))
+        print (ifail)
+        print (type(all_failures[ifail]))
+        print (all_failures[ifail])
+        print ("\n")
+
+    return (100. - (nb_errors*100./nb_values_total))
+
 
 def iterable_are_equal (item1, item2, comparison_path):
     keys_to_avoid = []
     common_keys = []
+
+    global nb_values_total
+    global nb_errors
     # print (comparison_path)
 
     if (type (item1) not in known_types or type(item2) not in known_types):
-        print (comparison_path + " " + str(type(item1)) + " " + str(type(item2)))
+        print (comparison_path + " " + str(type(item1)) + " " + str(type(item2)) + " are not in KNOWN Types\n")
 
     #############   NUMPY.NPZ.Files  #################
     # Convert npz files into compatible arrays
@@ -134,12 +153,17 @@ def iterable_are_equal (item1, item2, comparison_path):
 
 
     # If item1 and item2 are not iterable (are values)
-    elif (item1 != item2):
-        all_failures[str(comparison_path+str(type(item1))+"->"+str(item1))] = "(delta= TODO)"
+    else :
+        nb_values_total += 1
+        # if values are not equal
+        if (item1 != item2):
+            all_failures[str(comparison_path+str(type(item1))+"->"+str(item1))] = "(delta= TODO)"
+            nb_errors += 1
 
 
 def npz_values (f1_path, f2_path):
     ## TODO
+
 
     ## Check if both files are NPZ files
     if ((not f1_path.endswith(".npz")) or (not f2_path.endswith(".npz"))):
@@ -155,4 +179,5 @@ def npz_values (f1_path, f2_path):
             # Print failures Line-by-Line
             print(json.dumps(all_failures, indent=4))
 
-    
+    ratio =  compute_ratio()
+    print ("Ratio = " + str(ratio) + " %")
