9e9577ff7fd66c07946eb0026bbb97e2dfb17739
fbonnier <florent.bonnier@cnrs.fr>
2023-04-11 01:33:59 +0200

    WIP compute score

--- file_comparison/report_generator.py
+++ file_comparison/report_generator.py
@@ -8,10 +8,10 @@ error_diff_types = ["type", "len"]
 
 def compute_1el_difference (item1, item2):
     block_diff_1el = {"type1": str(type(item1)), "value1": item1, "type2": str(type(item1)), "value2": item2, "delta": None, "levenshtein": None, "rmse": None, "mse": None, "ape": None, "error": None}
-    # Test types: if types are different, return error type
-    if type(item1) != type(item2):
-        block_diff_1el["error"] = "Values are not the same type"
-        return (block_diff_1el)
+    # # Test types: if types are different, return error type
+    # if type(item1) != type(item2):
+    #     block_diff_1el["error"] = "Values are not the same type"
+    #     return (block_diff_1el)
 
     # # Test delta
     # # Compute Absolute difference between two values
