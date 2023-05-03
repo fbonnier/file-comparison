d1af5c2d4b393abbb70c555fc59bbfd07bb07a6d
fbonnier <florent.bonnier@cnrs.fr>
2023-05-03 20:18:08 +0200

    WIP compute hash

--- file_comparison/method.py
+++ file_comparison/method.py
@@ -73,13 +73,14 @@ class Method:
         try:
             with open(self.original_file, "rb") as foriginal, open(self.new_file, "rb") as fnew:
                 original_hash = Nilsimsa (foriginal.read())
-                new_hash = Nilsimsa (fnew.read())
-                score_nilsimsa = compare_digests (original_hash.hexdigest(), new_hash.hexdigest())
-                print(score_nilsimsa)
-                ratio = file_comparison.nilsimsa.compute_ratio (score_nilsimsa)
-                print(ratio)
-                self.hash_score = ratio*100.
-                print(self.hash_score)
+                print (original_hash)
+                # new_hash = Nilsimsa (fnew.read())
+                # score_nilsimsa = compare_digests (original_hash.hexdigest(), new_hash.hexdigest())
+                # print(score_nilsimsa)
+                # ratio = file_comparison.nilsimsa.compute_ratio (score_nilsimsa)
+                # print(ratio)
+                # self.hash_score = ratio*100.
+                # print(self.hash_score)
         except Exception as e:
             self.number_of_errors += 1
             self.errors.append("compare_hash error: " + str(e))
