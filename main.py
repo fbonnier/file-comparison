63ad6122112b1cf06afbbbd4aaddf740eabef2bd
fbonnier <florent.bonnier@cnrs.fr>
2023-06-27 15:55:27 +0200

    Fix final report

--- main.py
+++ main.py
@@ -66,14 +66,14 @@ def run_file_comparison_json (jsonfile, jsonfile_out):
     # Write data in JSON file
     with open(jsonfile_out, "w") as f:
         json_data_out = {}
-        json_data_out["Reusability Verification"] = {}
+        json_data_out["Reusability Verification"] = {"errors": [], "score": 0, "advice": [], "log": [], "report": {}}
         if error_glob:
             json_data_out["Reusability Verification"]["error"].append (error_glob)
         for ipair in pairs:
             if ipair["error"]:
                 json_data_out["Reusability Verification"]["error"].append (ipair["error"])
     
-        json_data_out["Reusability Verification"]["files"] = pairs
+        json_data_out["Reusability Verification"]["report"] = pairs
     
         # Methods report
         json.dump(json_data_out, f, indent=4)
